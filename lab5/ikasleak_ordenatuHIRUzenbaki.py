b3e7cb2ba402df3007228ffb1149fc72c1e88b79
Jon Inazio <sanchez@ingartek.com>
2016-10-18 09:23:11 +0200

    2. ariketa gehituta
    Ordenatu handienetik txikienerako algoritmoa badabil. Proba kasuak falta
    dira.

--- lab5/ikasleak_ordenatuHIRUzenbaki.py
+++ lab5/ikasleak_ordenatuHIRUzenbaki.py
@@ -1,16 +1,28 @@
-def trukatu (????):
- 
+def trukatu (a, b):
+    aux = b
+    b = a
+    a = aux
     return a,b
 
-def ordenatuBI (???):
-
+def ordenatuBI (a, b):
+    if a < b:
+        a,b=trukatu(a, b)
     return a,b
 
+#
+# Hiru zenbaki ordenatzen ditu handienetik txikienera.
+#
 def ordenatuHIRU (a, b, c):
-    ???=ordenatuBI(a, b)
-    ???=ordenatuBI(b, c)
-    ???=ordenatuBI(a, c)
-    return ???
+    # Pasadak honela jarri
+    atera = False
+    while not atera:
+        a,b=ordenatuBI(a, b)
+        b,c=ordenatuBI(b, c)
+        a,c=ordenatuBI(a, c)
+        if a >= b & b >= c:
+            atera = True
+    
+    return a,b,c
 
 def proba():
 	#guztiz desordenatuta
@@ -20,8 +32,7 @@ def proba():
 	n3=7;
 	n1,n2,n3=ordenatuHIRU(n1,n2,n3)
 	print "2,5,7 Zuen programak idatzi behar du 7 5 2 eta idazten du: %d %d %d" %(n1,n2,n3);
-    
-  
+
 	# 2, 7, 5
 	n1=2;
 	n2=7;
@@ -56,8 +67,5 @@ def proba():
 	n3=5;
 	n1,n2,n3=ordenatuHIRU(n1,n2,n3)
 	print "2,7,5 Zuen programak idatzi behar du 7 5 2 eta idazten du: %d %d %d" %(n1,n2,n3);
-    
-    
-    
 
 proba()
